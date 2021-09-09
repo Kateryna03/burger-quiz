4e16ad885a5f98e3f88b266c405efd68283f0d43
Kateryna Gurienkova <gurienkova@gmail.com>
2021-09-09 09:17:16 +0300

    +question/answer in modal

---
+++ script/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    const btnOpenModal = document.querySelector('#btnOpenModal');
+    const modalBlock = document.querySelector('#modalBlock');
+    const closeModal = document.querySelector('#closeModal');
+    const questionTitle = document.querySelector('#question');
+    const formAnswers = document.querySelector('#formAnswers');
+    const burgerName = 'Стандарт';
+    const burgerImageUrl = '../image/burger.png';
+
+    btnOpenModal.addEventListener('click', () => {
+        modalBlock.classList.add('d-block');
+        playTest();
+    });
+
+    closeModal.addEventListener('click', () => {
+        modalBlock.classList.remove('d-block');
+    });
+
+    const playTest = () => {
+        const renderQuestions = () => {
+            questionTitle.textContent = 'Какого цвета бургер Вы хотите?';
+            formAnswers.innerHTML = `
+                <div class="answers-item d-flex flex-column">
+                    <input type="radio" id="answerItem1" name="answer" class="d-none">
+                    <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src="${burgerImageUrl}" alt="burger">
+                        <span>${burgerName}</span>
+                    </label>
+                </div>
+            `
+        };
+        return renderQuestions();    
+    };
+});
+
+
+// событие DOMContentLoaded обрабатывает js когда загрузился весь html:
+//dir выводит элемент как объект
+//console.dir(btnOpenModal);
